d4b9beb411c86953c4a648808eda86ca1622109a
mason <mkaydo@bsd220.org>
2026-04-17 10:16:59 -0500

    Initial Commit

---
+++ Bowling With Lebron/Bowling_With_LebronApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bowling_With_LebronApp.swift
+//  Bowling With Lebron
+//
+//  Created by mason kaydo on 4/17/26.
+//
+
+import SwiftUI
+
+@main
+struct Bowling_With_LebronApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
