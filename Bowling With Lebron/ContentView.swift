1d02fd552909402bd62f2f8936d7de5f11101ed7
Joseph <jjphillipse4@gmail.com>
2026-05-15 10:34:55 -0500

    background

--- Bowling With Lebron/ContentView.swift
+++ Bowling With Lebron/ContentView.swift
@@ -1,70 +1,78 @@
-//
-//  ContentView.swift
-//  Bowling With Lebron
-//
-//  Created by mason kaydo on 4/17/26.
-//
-
 import SwiftUI
 import AVKit
 import AVFoundation
 
 struct ContentView: View {
-    
+
     @State private var showVideo = false
-    
+
     var body: some View {
-        NavigationView{
-            VStack {
-                
-                Image("Lebron")
+
+        NavigationView {
+
+            ZStack {
+
+                // background image
+                Image("23")
                     .resizable()
-                    .scaledToFit()
-                NavigationLink(destination: MemePage()) {
-                    Text("See who the GOAT is")
-                        .font(.headline)
-                        .foregroundColor(.purple)
-                        .padding()
-                        .background(Color.yellow)
-                        .clipShape(RoundedRectangle(cornerRadius: 12))
-                        .overlay(
-                    RoundedRectangle(cornerRadius: 12)
-                        .stroke(Color.purple, lineWidth: 3)
+                    .scaledToFill()
+                    .ignoresSafeArea()
+
+                VStack(spacing: 30) {
+
+                    Image("Lebron")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(height: 300)
+
+                    NavigationLink(destination: MemePage()) {
+
+                        Text("See who the GOAT is")
+                            .font(.headline)
+                            .foregroundColor(.purple)
+                            .padding()
+                            .background(Color.yellow)
+                            .clipShape(RoundedRectangle(cornerRadius: 16))
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 12)
+                                    .stroke(Color.purple, lineWidth: 3)
+                            )
+                    }
+
+                    Button("Ba Boom Boom Boom") {
+                        showVideo = true
+                    }
+                    .padding()
+                    .font(.headline)
+                    .foregroundColor(.purple)
+                    .background(Color.yellow)
+                    .clipShape(RoundedRectangle(cornerRadius: 12))
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 12)
+                            .stroke(Color.purple, lineWidth: 3)
                     )
                 }
-                Button("Ba Boom Boom Boom") {
-                    showVideo = true
-                }
-                .padding()
-                .font(.headline)
-                .foregroundColor(.purple)
-                .background(Color.yellow)
-                .clipShape(RoundedRectangle(cornerRadius: 12))
-                .overlay(
-                    RoundedRectangle(cornerRadius: 12)
-                        .stroke(Color.purple, lineWidth: 3)
-                )
             }
             .fullScreenCover(isPresented: $showVideo) {
                 VideoPlayerView()
             }
         }
     }
-    
+
     struct VideoPlayerView: View {
-        
+
         let player: AVPlayer = {
-            
+
             guard let url = Bundle.main.url(forResource: "lebronVideo", withExtension: "MP4") else {
                 fatalError("Video not found")
             }
-            
+
             return AVPlayer(url: url)
-            
+
         }()
-        
+
         var body: some View {
-            
+
             VideoPlayer(player: player)
                 .ignoresSafeArea()
                 .onAppear {
@@ -73,6 +81,7 @@ struct ContentView: View {
         }
     }
 }
+
 #Preview {
     ContentView()
 }
